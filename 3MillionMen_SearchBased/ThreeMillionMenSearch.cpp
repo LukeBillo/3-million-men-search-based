c0e5c863f7e8746498b97960c4cb879e07c1ca8d
LukeBillo <n0627514@ntu.ac.uk>
2019-02-25 18:38:11 +0000

    Optimisations and such

--- 3MillionMen_SearchBased/ThreeMillionMenSearch.cpp
+++ 3MillionMen_SearchBased/ThreeMillionMenSearch.cpp
@@ -5,50 +5,63 @@
 #include <algorithm>
 
 
-three_million_men_search::three_million_men_search(std::shared_ptr<std::map<std::string, std::string>>& name_mapping)
+three_million_men_search::three_million_men_search(std::unique_ptr<name_map>& name_mapping)
 {
-	_names = name_mapping;
+	_names_by_key = std::move(name_mapping);
+
+	_names_by_value = std::make_unique<name_map>();
+	for (auto i = _names_by_key->begin(); i != _names_by_key->end(); ++i)
+	{
+		_names_by_value->operator[](i->second) = i->first;
+	}
+
 	_results = std::make_unique<std::list<std::string>>();
 }
 
 void three_million_men_search::run()
 {
-	const auto starting_point = _names->begin();
+	const auto starting_point = _names_by_key->begin();
 	run_recursive_west(starting_point);
-	run_recursive_east(starting_point);
+
+	const auto starting_point_east = _names_by_value->find(_results->front());
+	if (starting_point_east == _names_by_value->end())
+		return;
+
+	run_recursive_east(starting_point_east);
 }
 
-void three_million_men_search::run_recursive_west(std::map<std::string, std::string>::iterator current_name_pair)
+void three_million_men_search::run_recursive_west(name_map::iterator current_name_pair)
 {
 	_results->push_back(current_name_pair->first);
-	const auto west_neighbour = _names->find(current_name_pair->second);
+	const auto west_neighbour = _names_by_key->find(current_name_pair->second);
 
 	// Reached western-most person, has no neighbour
 	// as this is the back of the line.
 	// Time to return East from the start.
-	if (west_neighbour == _names->end())
+	if (west_neighbour == _names_by_key->end())
 	{
 		_results->push_back(current_name_pair->second);
 		return;
 	}
 
+	_names_by_key->erase(current_name_pair);
+
 	return run_recursive_west(west_neighbour);
 }
 
-void three_million_men_search::run_recursive_east(std::map<std::string, std::string>::iterator current_name_pair)
+void three_million_men_search::run_recursive_east(name_map::iterator current_name_pair)
 {
-	// iterator init, allows return to be outside of for loop
-	// for tail call optimisation
-	const auto east_neighbour = std::find_if(_names->begin(), _names->end(), 
-		[current_name_pair](const std::pair<std::string, std::string> name) { return name.second == current_name_pair->first; });
+	_results->push_front(current_name_pair->second);
 
-	if (east_neighbour == _names->end())
+	const auto east_neighbour = _names_by_value->find(current_name_pair->second);
+	_names_by_value->erase(current_name_pair);
+
+	if (east_neighbour == _names_by_value->end())
 	{
 		// end of the line!
 		return;
 	}
 
-	_results->push_front(east_neighbour->first);
 	return run_recursive_east(east_neighbour);
 }
 
--- 3MillionMen_SearchBased/ThreeMillionMenSearch.h
+++ 3MillionMen_SearchBased/ThreeMillionMenSearch.h
@@ -1,22 +1,26 @@
 #pragma once
 #include <memory>
-#include <map>
 #include <list>
+#include <unordered_map>
 
 class three_million_men_search
 {
 public:
-	three_million_men_search(std::shared_ptr<std::map<std::string, std::string>>&);
+	using name_map = std::unordered_map<std::string, std::string>;
+	using name_list = std::list<std::string>;
+
+	three_million_men_search(std::unique_ptr<name_map>&);
 	~three_million_men_search() = default;
 
 	void run();
 	std::list<std::string>* get_results();
 
 private:
-	std::shared_ptr<std::map<std::string, std::string>> _names;
-	std::unique_ptr<std::list<std::string>> _results;
+	std::unique_ptr<name_map> _names_by_key;
+	std::unique_ptr<name_map> _names_by_value;
+	std::unique_ptr<name_list> _results;
 
-	void run_recursive_west(std::map<std::string, std::string>::iterator current_name_pair);
-	void run_recursive_east(std::map<std::string, std::string>::iterator current_name_pair);
+	void run_recursive_west(name_map::iterator current_name_pair);
+	void run_recursive_east(name_map::iterator current_name_pair);
 };
 
